c0c29070a9df81319717ecf62251f597cba506b4
Amélie Strathmann <amelie.strathmann@tu-dortmund.de>
2024-05-05 15:11:29 +0200

    Berechnung angefangen

--- V21/plot.py
+++ V21/plot.py
@@ -6,3 +6,7 @@ import uncertainties.unumpy as unp
 from uncertainties.unumpy import nominal_values as noms, std_devs as stds, uarray as uar
 from uncertainties import ufloat
 
+#Magnetfeld der Erde
+#funktion zur Berechnung des Magnetfeldes
+def B(I, n, R):
+    return const.mu_0 * (8 * I * n) / (np.sqrt(125) * R)
