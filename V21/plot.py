e7072c3f2baaff25294f071c1a68752053325465
Amélie Strathmann <amelie.strathmann@tu-dortmund.de>
2024-05-05 15:04:27 +0200

    auswertung überschriften erstellt

--- V21/plot.py
+++ V21/plot.py
@@ -5,3 +5,4 @@ import scipy.constants as const
 import uncertainties.unumpy as unp
 from uncertainties.unumpy import nominal_values as noms, std_devs as stds, uarray as uar
 from uncertainties import ufloat
+
